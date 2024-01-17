59d15e875e4278b104f573e76b752841b7549e90
rydnr <github@acm-sl.org>
2024-01-17 19:16:20 +0100

    Added vim and emacs settings

--- pythoneda/realm/rydnr/__init__.py
+++ pythoneda/realm/rydnr/__init__.py
@@ -26,3 +26,11 @@ try:
     from pythoneda.realm.rydnr import Rydnr
 except ImportError:
     pass
+# vim: syntax=python ts=4 sw=4 sts=4 tw=79 sr et
+# Local Variables:
+# mode: python
+# python-indent-offset: 4
+# tab-width: 4
+# indent-tabs-mode: nil
+# fill-column: 79
+# End:
--- pythoneda/realm/rydnr/infrastructure/cli/__init__.py
+++ pythoneda/realm/rydnr/infrastructure/cli/__init__.py
@@ -22,3 +22,11 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
 from .change_staging_code_request_delegated_cli import ChangeStagingCodeRequestDelegatedCli
+# vim: syntax=python ts=4 sw=4 sts=4 tw=79 sr et
+# Local Variables:
+# mode: python
+# python-indent-offset: 4
+# tab-width: 4
+# indent-tabs-mode: nil
+# fill-column: 79
+# End:
